c0fd618b62f2fce7bc6a838d97fdcfb74aa11f33
prasanthiarepalli51-ai <prasanthiarepalli51@gmail.com>
2026-07-18 05:43:15 +0530

    Fix output message for non-palindrome case
    Easy to write a code in a simple lines

---
+++ Palindrome.py
@@ -0,0 +1,13 @@
+#palindrome means a number will write left to right or right to left the number issame it is palindrome.
+#EX:12321
+#program:
+n = input() #user input
+if n == n[::-1]: #condition
+    print("Palindrome") #the condition is true 
+else:
+    print("Not Palindrome") # condition is falseu
+
+
+ouput:
+12321
+palindrome 
